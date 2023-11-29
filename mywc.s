a4d4a2a258b1922e6f3a11372aa7d31fe9cf1667
matthewzhang <mz8189@princeton.edu>
2023-11-29 01:47:16 -0500

    edit testing

--- mywc.s
+++ mywc.s
@@ -16,38 +16,41 @@ iInWord:
 .section .text
 .global main
 main:
-    sub sp, sp, 16  // Stack frame setup
-    str x30, [sp]   // Save return address
+    sub sp, sp, 16    // Stack frame setup
+    str x30, [sp]     // Save return address
 
     // Initialize iInWord to FALSE (0)
-    mov w2, 0
-    str w2, [sp, 12]
+    mov w1, 0
+    str w1, [sp, 12]
 
 loop:
-    bl getchar
-    cmp w0, -1  // Check for EOF
+    bl getchar        // Call getchar
+    cmp w0, -1        // Check for EOF
     beq endloop
+    mov w1, w0        // Store character in w1
 
     // Increment lCharCount
-    ldr x1, =lCharCount
-    ldr x2, [x1]
-    add x2, x2, 1
-    str x2, [x1]
+    ldr x2, =lCharCount
+    ldr x3, [x2]
+    add x3, x3, 1
+    str x3, [x2]
 
-    // Check if character is whitespace
+    // Check if character is a whitespace
     bl isspace
-    cmp w0, 0
+    cmp w0, 1
     beq char_is_space
 
     // Character is not a space
     ldr w2, [sp, 12]
     cmp w2, 0
-    bne not_first_word
-    ldr x1, =lWordCount
-    ldr x2, [x1]
-    add x2, x2, 1
-    str x2, [x1]
-    mov w2, 1  // Set iInWord to TRUE
+    bne loop          // Continue if already in a word
+
+    // Starting a new word
+    ldr x2, =lWordCount
+    ldr x3, [x2]
+    add x3, x3, 1
+    str x3, [x2]
+    mov w2, 1         // Set iInWord to TRUE
     str w2, [sp, 12]
     b loop
 
@@ -55,30 +58,27 @@ char_is_space:
     // Character is a space, set iInWord to FALSE
     mov w2, 0
     str w2, [sp, 12]
-    cmp w0, 10  // Check if newline
+    cmp w1, 10        // Check if newline
     bne loop
 
-    // Increment lLineCount
-    ldr x1, =lLineCount
-    ldr x2, [x1]
-    add x2, x2, 1
-    str x2, [x1]
+    // Increment lLineCount for newline
+    ldr x2, =lLineCount
+    ldr x3, [x2]
+    add x3, x3, 1
+    str x3, [x2]
     b loop
-
-not_first_word:
-    b loop  // Continue the loop
 
 endloop:
     // Check iInWord for last word
     ldr w2, [sp, 12]
-    cmp w2, 0
-    beq end
+    cmp w2, 1
+    bne end
 
-    // Increment lWordCount
-    ldr x1, =lWordCount
-    ldr x2, [x1]
-    add x2, x2, 1
-    str x2, [x1]
+    // Increment lWordCount for last word
+    ldr x2, =lWordCount
+    ldr x3, [x2]
+    add x3, x3, 1
+    str x3, [x2]
 
 end:
     // Print results
