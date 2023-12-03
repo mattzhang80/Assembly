9e0cadfd814db00a078db2688cac765e46e1e245
matthewzhang <mz8189@princeton.edu>
2023-12-03 15:39:59 -0500

    bigintadd.s

--- bigintadd.s
+++ bigintadd.s
@@ -24,19 +24,20 @@
         mov x29, sp
 
         // Load in lengths of both bigints
-        ldr w3, [x0, LLENGTH_OFFSET]
-        ldr w4, [x1, LLENGTH_OFFSET]
+        ldr w3 [x0, LLENGTH_OFFSET]
+        ldr w4 [x1, LLENGTH_OFFSET]
 
         cmp w3, w4
         blt len_w4_greater
         mov w5, w3
+
         b continue
 
     len_w4_greater:
         mov w5, w4
     
     continue:
-        // Initialize lIndex and ulCarry respectively
+        //initialize lIndex and ulCarry respectively
         mov w6, #0
         mov w7, #0
 
@@ -46,15 +47,15 @@
         bge loop_end
 
         // Load in digit of both bigints
-        ldr x8, [x0, DIGITS_OFFSET]
-        ldr x9, [x1, DIGITS_OFFSET]
+        ldr x8, [x0, DIGITS_OFFSET, lsl #3]
+        ldr x9, [x1, DIGITS_OFFSET, lsl #3]
 
         // Add digits together
         adds x8, x8, x9
-        adc w7, wzr, wzr  // Use wzr for the third operand
+        adc w7, w7, #0
 
         // Store digit in x2
-        str x8, [x2, DIGITS_OFFSET]
+        str x8, [x2, DIGITS_OFFSET, lsl #3]
 
         // Increment lIndex
         add w6, w6, #1
@@ -68,11 +69,11 @@
         cmp w5, #MAX_DIGITS
         beq overflow
         mov x8, #1
-        str x8, [x2, DIGITS_OFFSET]
+        str x8, [x2, DIGITS_OFFSET, lsl #3]
         add w5, w5, #1
 
     end:
-        str w5, [x2, LLENGTH_OFFSET]
+        str w5 [x2, LLENGTH_OFFSET]
         mov w0, TRUE
         b epilogue
 
