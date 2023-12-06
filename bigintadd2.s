845c27c5ef66325cbe128daf922b6b7b1d7a1c02
matthewzhang <mz8189@princeton.edu>
2023-12-05 19:02:57 -0500

    weird

--- bigintadd2.s
+++ bigintadd2.s
@@ -12,7 +12,7 @@
 
 //----------------------------------------------------------------------
 .section .text
-    .equ    ADD_STACK_BYTECOUNT, 64
+    .equ    ADD_STACK_BYTECOUNT, 96
 
     // Constants
     .equ    TRUE, 1
